6ef1990e2a6318eefc16d4d5cdf1872162e0582c
kartikjain-sudo <kartikjain42@protonmail.com>
2022-12-28 23:25:22 +0530

    fix: resolved bug

--- src/main.rs
+++ src/main.rs
@@ -14,17 +14,25 @@ trait VehicleInfo {
         }
     }
     fn get_vehicle_info(&self) -> String;
-    fn get_year(&self) -> i32 {
-        self.year
-    }
+    fn get_year(&self) -> i32;
 }
 
 impl VehicleInfo for Vehicle {
     fn get_vehicle_info(&self) -> String {
         format!("{} {} {}", self.class, self.model, self.year)
     }
+
+    fn get_year(&self) -> i32 {
+        self.year
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    let car = Vehicle {
+        class: String::from("S class"),
+        model: String::from("BMW X5"),
+        year: 2019,
+    };
+    print!("{}", car.get_vehicle_info());
 }
