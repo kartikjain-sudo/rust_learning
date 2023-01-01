efb25b445d2210d3638cfc1bbf97404d9939a795
kartikjain-sudo <kartikjain42@protonmail.com>
2023-01-01 20:02:06 +0530

    feat: string input from user

--- learning/variables/string.rs
+++ learning/variables/string.rs
@@ -3,8 +3,12 @@ use std::io;
 fn read_string() {
     let mut input = String::new();
     println!("Enter your name");
+    
     io::stdin().read_line(&mut input).expect("string is expected");
-
+    
+    // input = input.trim().to_string();
+    // index.push_str(", ok");
+    
     println!("{}", input);
 }
 
